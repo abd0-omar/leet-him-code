3fb0edded030e29a18212faefde644bf7cf04a8f
Abdo Omar <abdelrahmanorigin@gmail.com>
2024-05-27 14:31:08 +0300

    optimize solution
    searching for the first num bigger than or equal me, then checking if
    the len of the arra - the found number is equal to the x

--- special-array-with-x-elements-greater-than-or-equal-x/src/lib.rs
+++ special-array-with-x-elements-greater-than-or-equal-x/src/lib.rs
@@ -1,13 +1,50 @@
 pub fn special_array(mut nums: Vec<i32>) -> i32 {
     nums.sort_unstable();
-    for x in 0..1000 {
-        let bigger_than_x = nums.iter().filter(|f| **f >= x as i32).count();
-        if bigger_than_x == x {
-            return x as _;
+    // find first num >= me
+
+    // for x in 0..1000 {
+    for x in 0..=nums.len() {
+        let first_num_bigger_than_me = search_first_num_bigger_than_me(&nums, x as i32);
+        println!("x={:?}", x);
+        println!("first_num_bigger_than_me={:?}", first_num_bigger_than_me);
+        if let Some(first_bigger) = first_num_bigger_than_me {
+            if x as i32 == nums.len() as i32 - first_bigger {
+                return x as _;
+            }
         }
     }
+
     -1
 }
+
+fn search_first_num_bigger_than_me(nums: &[i32], x: i32) -> Option<i32> {
+    let mut l = 0i32;
+    let mut r = nums.len() as i32 - 1;
+    let mut ans = None;
+
+    while l <= r {
+        let mid = l + (r - l) / 2;
+
+        if nums[mid as usize] >= x {
+            r = mid - 1;
+            ans = Some(mid);
+        } else {
+            l = mid + 1;
+        }
+    }
+    ans
+}
+
+// pub fn special_array(mut nums: Vec<i32>) -> i32 {
+//     nums.sort_unstable();
+//     for x in 0..1000 {
+//         let bigger_than_x = nums.iter().filter(|f| **f >= x as i32).count();
+//         if bigger_than_x == x {
+//             return x as _;
+//         }
+//     }
+//     -1
+// }
 
 #[cfg(test)]
 mod tests {
