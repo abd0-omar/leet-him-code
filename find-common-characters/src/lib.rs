6718af74926af166b26a05e3073c8c048692cf0c
Abdo Omar <abdelrahmanorigin@gmail.com>
2024-06-05 13:17:42 +0300

    cleanup

--- find-common-characters/src/lib.rs
+++ find-common-characters/src/lib.rs
@@ -1,94 +1,34 @@
 pub fn common_chars(words: Vec<String>) -> Vec<String> {
     let mut freq = vec![vec![0; 26]; words.len()];
+    //  a  b  c    a   b  c   a  b
+    //[[1, 1, 1], [1, 1, 1], [1, 1, 0]]
     for (i, word) in words.iter().enumerate() {
-        // println!("freq={:?}", freq);
         for letter in word.chars() {
             let letter_idx = (letter as u8 - b'a') as usize;
             freq[i][letter_idx] += 1;
         }
     }
-    println!("freq={:?}", freq);
+
     let mut common_chars = Vec::new();
 
-    let mut idx = 0;
-    while idx < 26 {
-        // let mut prev = None;
-        let mut all_same = true;
+    for idx in 0..26 {
         let mut min_repetation = i32::MAX;
         for i in 0..freq.len() {
-            // print!("{}, ", freq[i][idx]);
             let cur = freq[i][idx];
-            min_repetation = min_repetation.min(cur);
             if cur < 0 {
-                all_same = false;
                 break;
             }
-            // match prev {
-            //     Some(p) => {
-            //         if {
-            //             all_same = false;
-            //             break;
-            //         }
-            //         prev = Some(cur);
-            //     }
-            //     None => {
-            //         if cur == 0 {
-            //             all_same = false;
-            //             break;
-            //         }
-            //         prev = Some(cur)
-            //     }
-            // };
+            min_repetation = min_repetation.min(cur);
         }
-        // println!();
-        if all_same {
+
+        if min_repetation != i32::MAX {
             let letter = (char::from(idx as u8 + b'a')).to_string();
             for _ in 0..min_repetation {
                 common_chars.push(letter.clone());
             }
         }
-        idx += 1
     }
 
-    // for f in 0..freq.len() {
-    //     let mut are_equal = true;
-    //     let mut prev = None;
-    //     let mut idx = 0;
-    //     // [[1, 1], [1, 1], [1, 0]]
-    //     for i in 0..freq[f].len() {
-    //         idx = i;
-    //         match prev {
-    //             Some(p) => {
-    //                 if p != k {
-    //                     are_equal = false;
-    //                     break;
-    //                 }
-    //                 prev = Some(k);
-    //             }
-    //             None => prev = Some(k),
-    //         }
-    //     }
-    //     if are_equal {
-    //         let letter = (idx as u8 + b'a').to_string();
-    //         common_chars.push(letter);
-    //     }
-    // }
-    //
-    // for (key, val) in freq {
-    //     // if the val is greater than or equal words.len(), then it's common
-    //     let letter_count = val / words.len();
-    //     println!("letter_count={:?}", letter_count);
-    //     println!("key={:?}", key);
-    //     println!("val={:?}", val);
-    //     println!("words={:?}", words.len());
-    //     // letter_count=1
-    //     // key='a'
-    //     // val=14
-    //     // words=8
-    //     for _ in 0..letter_count {
-    //         common_chars.push(key.to_string());
-    //     }
-    // }
     common_chars
 }
 
