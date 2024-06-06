99490a84d6ab690ba547bc35d4d931f5f4abdd7d
Abdo Omar <abdelrahmanorigin@gmail.com>
2024-06-06 20:34:21 +0300

    freelo
    2 for 1 combo solve hand-of-straights and get this one with it for
    FREE!!!11

--- divide-array-in-sets-of-k-consecutive-numbers/src/lib.rs
+++ divide-array-in-sets-of-k-consecutive-numbers/src/lib.rs
@@ -1,13 +1,13 @@
-pub fn is_n_straight_hand(hand: Vec<i32>, group_size: i32) -> bool {
+pub fn is_possible_divide(nums: Vec<i32>, k: i32) -> bool {
     // 440
     // let mut freq = vec![0; max_num as usize + 1];
-    // hand.sort_unstable();
+    // nums.sort_unstable();
     // println!("freq={:?}", freq);
     // for num in hand {
     //     freq[num as usize] += 1;
     // }
     let mut freq = std::collections::HashMap::new();
-    for &num in hand.iter() {
+    for &num in nums.iter() {
         freq.entry(num)
             .and_modify(|counter| *counter += 1)
             .or_insert(1);
@@ -15,10 +15,10 @@ pub fn is_n_straight_hand(hand: Vec<i32>, group_size: i32) -> bool {
 
     let mut freq_vec: Vec<_> = freq.into_iter().collect();
     freq_vec.sort_unstable();
-    println!("freq_vec={:?}", freq_vec);
+    // println!("freq_vec={:?}", freq_vec);
 
     'outer: loop {
-        let mut count = group_size;
+        let mut count = k;
         let mut prev: Option<i32> = None;
         let mut all_zeros = true;
         for (num, freq) in freq_vec.iter_mut() {
@@ -111,86 +111,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works0() {
-        // freq array
-        // 0, 1, 2, 3, 4, 5, 6, 7, 8
-        //[0, 1, 2, 2, 1, 0, 1, 1, 1]
-        //[0, 0, 1, 1, 1, 0, 1, 1, 1]
-        //[0, 0, 0, 0, 0, 0, 1, 1, 1]
-
-        // 0, 1, 2, 3, 4, 5
-        //[0, 1, 1, 1, 1, 1]
-        //[0, 0, 0, 0, 1, 1]
-        let hand = vec![1, 2, 3, 6, 2, 3, 4, 7, 8];
-        let group_size = 3;
-        let output = true;
-        // Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
-
-    #[test]
-    fn it_works1() {
-        // 0, 1, 2
-        //[0, 1, 1]
-        //[0, 0, 0]
-        //
-        //it doesn't have to be group_size groups
-        //but leetcode said that
-        let hand = vec![2, 1];
-        let group_size = 2;
-        let output = true;
-        // groups [1, 2], [empty]
-        // so false
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
-
-    #[test]
-    fn it_works2() {
-        // 0, 1, 2, 3
-        //[0, 2, 2, 2]
-        //[0, 1, 1, 2]
-        //[0, 0, 0, 1]
-        //
-        // 1 | 2
-        // 2 | 2
-        // 3 | 2
-        //
-        let hand = vec![1, 1, 2, 2, 3, 3];
-        let group_size = 2;
-        let output = false;
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
-
-    #[test]
-    fn it_works3() {
-        let hand = vec![1, 2, 3, 4, 5, 6];
-        let group_size = 2;
-        let output = true;
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
-
-    #[test]
-    fn it_works4() {
-        let hand = vec![0, 1];
-        let group_size = 2;
-        let output = true;
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
-
-    #[test]
-    fn it_works5() {
-        let hand = vec![
-            233722108, 386144634, 221638886, 175028874, 408337082, 91410299, 793763682, 972910825,
-            627251147, 135020779,
-        ];
-        let group_size = 2;
-        let output = true;
-        let result = is_n_straight_hand(hand, group_size);
-        assert_eq!(result, output)
-    }
+    fn it_works() {}
 }
