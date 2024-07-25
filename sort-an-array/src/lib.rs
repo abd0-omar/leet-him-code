0e0a7b6e56cfa9f33c1b7af5ea05ac52a927778f
Abdo Omar <abdelrahmanorigin@gmail.com>
2024-07-25 15:00:53 +0300

    merge sort with inversion count

--- sort-an-array/src/lib.rs
+++ sort-an-array/src/lib.rs
@@ -1,9 +1,87 @@
 pub fn sort_array(mut nums: Vec<i32>) -> Vec<i32> {
     let n = nums.len();
-    quick_sort(&mut nums, 0, n - 1);
-    nums
+    if n <= 1 {
+        return nums;
+    }
+    merge_sort(&nums)
 }
 
+// merge sort
+pub fn merge_sort(vec: &Vec<i32>) -> Vec<i32> {
+    let mut tmp = vec![-1; vec.len()];
+    let mut inversions_count = 0;
+    let mut vec_clone = vec.clone();
+    _merge_sort(
+        &mut vec_clone,
+        0,
+        vec.len() - 1,
+        &mut tmp,
+        &mut inversions_count,
+    );
+    tmp
+}
+
+fn _merge_sort(
+    vec: &mut Vec<i32>,
+    st: usize,
+    end: usize,
+    tmp: &mut Vec<i32>,
+    inversions_count: &mut i32,
+) {
+    if st >= end {
+        return;
+    }
+
+    let mid = st + (end - st) / 2;
+
+    _merge_sort(vec, st, mid, tmp, inversions_count);
+    _merge_sort(vec, mid + 1, end, tmp, inversions_count);
+
+    _join_sorted_arrays(vec, st, mid, end, tmp, inversions_count);
+}
+
+fn _join_sorted_arrays(
+    vec: &mut Vec<i32>,
+    st: usize,
+    mid: usize,
+    end: usize,
+    tmp: &mut Vec<i32>,
+    inversions_count: &mut i32,
+) {
+    let mut i = st;
+    let mut j = mid + 1;
+    let mut k = st;
+
+    while i <= mid && j <= end {
+        if vec[i] <= vec[j] {
+            tmp[k] = vec[i];
+            i += 1;
+        } else {
+            tmp[k] = vec[j];
+            j += 1;
+            *inversions_count += (mid + 1 - i) as i32;
+        }
+        k += 1;
+    }
+
+    while i <= mid {
+        tmp[k] = vec[i];
+        i += 1;
+        k += 1;
+    }
+
+    while j <= end {
+        tmp[k] = vec[j];
+        j += 1;
+        k += 1;
+    }
+
+    for index in st..=end {
+        vec[index] = tmp[index];
+    }
+}
+
+// basic random pivot quick sort
 fn quick_sort(nums: &mut Vec<i32>, low: usize, high: usize) {
     if low >= high {
         return;
