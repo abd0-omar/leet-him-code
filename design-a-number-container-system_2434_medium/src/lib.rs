3b945772a96e760df95ae0e615e2634e174cec3f
abdo <abdelrahmanorigin@gmail.com>
2025-02-08 10:14:08 +0200

    two hashmaps to trace index to number, and trace number to a set of ordered of indices
    in number_to_indices, remove number if the it's set becomes empty
    a good follow-up for yesterday's problem

---
+++ design-a-number-container-system_2434_medium/src/lib.rs
@@ -0,0 +1,56 @@
+// https://leetcode.com/problems/design-a-number-container-system/
+#[allow(dead_code)]
+struct Solution;
+
+use std::collections::{BTreeSet, HashMap};
+#[allow(dead_code)]
+struct NumberContainers {
+    // a good follow up for yesterday's problem
+    index_to_number: HashMap<i32, i32>,
+    number_to_indices: HashMap<i32, BTreeSet<i32>>,
+}
+
+/**
+ * `&self` means the method takes an immutable reference.
+ * If you need a mutable reference, change it to `&mut self` instead.
+ */
+impl NumberContainers {
+    fn new() -> Self {
+        Self {
+            index_to_number: HashMap::new(),
+            number_to_indices: HashMap::new(),
+        }
+    }
+
+    fn change(&mut self, index: i32, number: i32) {
+        if let Some(&old_number) = self.index_to_number.get(&index) {
+            if let Some(indices) = self.number_to_indices.get_mut(&old_number) {
+                indices.remove(&index);
+                if indices.is_empty() {
+                    self.number_to_indices.remove(&old_number);
+                }
+            }
+        }
+        self.index_to_number.insert(index, number);
+        self.number_to_indices
+            .entry(number)
+            .or_insert(BTreeSet::new())
+            .insert(index);
+    }
+
+    fn find(&self, number: i32) -> i32 {
+        // println!("{:?}", self.number_to_indices);
+        if let Some(b_tree_set) = self.number_to_indices.get(&number) {
+            *b_tree_set.first().unwrap_or(&-1)
+        } else {
+            -1
+        }
+    }
+}
+
+// /**
+//  * Your NumberContainers object will be instantiated and called as such:
+//  * let obj = NumberContainers::new();
+//  * obj.change(index, number);
+//  * let ret_2: i32 = obj.find(number);
+//  */
